4f1d8fc08d2f93c955cbf0d1a0b847e4641fb95c
Silvan Buedenbender <silvan.buedenbender@studitemps.de>
2021-11-08 18:04:57 +0100

    Wrap curl in loop with retries and add corresponding arguments

--- scan-website.sh
+++ scan-website.sh
@@ -1,15 +1,24 @@
 website_url=$1
 scan_text=$2
-status_code=$(curl --silent --location --head --output /dev/null --write-out "%{http_code}" "${website_url}")
+retries=${$3:-"3"}
+backoff_multiplier=${$4:-"2"}
 
-if [ "${status_code}" != "200" ]; then
-    echo "Website is unreachable (${status_code})" >&2
-    exit 1
-fi
+for i in {1..$retries}; do
+    status_code=$(curl --silent --location --head --output /dev/null --write-out "%{http_code}" "${website_url}")
 
-if ! curl --silent --location "{$website_url}" | grep --quiet --ignore-case "${scan_text}"; then
-    echo "Text was not found on website" >&2
-    exit 1
-fi
+    if [ "${status_code}" != "200" ]; then
+        echo "Website is unreachable (${status_code})" >&2
+        sleep $i * $backoff_multiplier
+        continue
+    fi
 
-exit 0
+    if ! curl --silent --location "{$website_url}" | grep --quiet --ignore-case "${scan_text}"; then
+        echo "Text was not found on website" >&2
+        sleep $i * $backoff_multiplier
+        continue
+    fi
+
+    exit 0
+done
+
+exit 1
